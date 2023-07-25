1ab0907a9fc063e16d34dae0ccd5c97f8b3c8484
PeaceOffiong <97194474+PeaceOffiong@users.noreply.github.com>
2023-07-25 08:15:53 +0000

    fixing Eslint issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head ><script src="https://smtpjs.com/v3/smtp.js"></script></Head>
+      <Head ><script defer src="https://smtpjs.com/v3/smtp.js"></script></Head>
       <body>
         <Main />
         <NextScript />
