7215eeae0c76f64e4c018a1fd34660a5fbb560f8
PeaceOffiong <97194474+PeaceOffiong@users.noreply.github.com>
2023-07-26 15:39:24 +0000

    fixing ESLints Requirements

--- src/customHooks/usefetchUser.tsx
+++ src/customHooks/usefetchUser.tsx
@@ -2,12 +2,8 @@ import { useState, useEffect } from "react";
 import { db } from '@/firebase/firebaseConfig';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 
-const fetchUser = (email:string) => {
-    const [loading, setLoading] = useState(true);
+const fetchUser = async (email:string) => {
     const [specificUser, setSpecificUser] = useState<any[]>([]);
-
-    useEffect(() => {
-        const getData = async () => {
             try {
                 const q = query(collection(db, 'Users'), where('email', '==', email));
                 const querySnapshot = await getDocs(q);
@@ -15,7 +11,6 @@ const fetchUser = (email:string) => {
                 if (!querySnapshot.empty) {
                     const userDoc = querySnapshot.docs[0];
                     const user = userDoc.data();
-                    setLoading(false)
                     setSpecificUser([user]);             
                 } else {
                     return null;
@@ -24,12 +19,8 @@ const fetchUser = (email:string) => {
                 console.error('Error getting user from Firestore:', error);
                 return null;
             }
-        };
-        
-        getData();
-    }, [email]);
 
-    return { loading, specificUser };
+    return  specificUser ;
 }
 
 export default fetchUser;
